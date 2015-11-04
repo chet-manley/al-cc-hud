902853029acfd69654ae4a48f9f2c42c51cfe268
Chet Manley <joshua.gardner+github@hotmail.com>
2015-11-04 04:59:25 -0600

    Added routes

--- app/alccDash.js
+++ app/alccDash.js
@@ -1,4 +1,16 @@
-(function (global, undefined) {
+(function (window, undefined) {
 	'use strict';
-	angular.module('alccDash', ['ngRoute']);
+	angular.module('alccDash', ['ngRoute'])
+		.config(function ($routeProvider) {
+			$routeProvider
+				.when('/', {
+					templateUrl: 'app/components/dashboard/dashboard.htm'
+				})
+				.when('/dashboard/:channel', {
+					templateUrl: 'app/components/dashboard/dashboard.htm'
+				})
+				.when('/scorecard/:cardname', {
+					templateUrl: 'app/components/scorecard/scorecard.htm'
+				});
+		});
 }(window || this));
