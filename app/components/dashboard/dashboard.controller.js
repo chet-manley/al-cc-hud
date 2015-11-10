20628fbe180e00425fe5ba563eacd969b0291ed0
Chet Manley <joshua.gardner+github@hotmail.com>
2015-11-09 22:55:37 -0600

    Added dashboard controller

---
+++ app/components/dashboard/dashboard.controller.js
@@ -0,0 +1,30 @@
+(function (angular, undefined) {
+	'use strict';
+	/* create controller */
+	function DashboardController($routeParams, data) {
+		var ctrl = this,
+			scorecard = data.scorecard(),
+			table = {},
+			optUpdate = function optUpdate(opt) {
+				if (opt === 'One Month') {
+					table.headers = scorecard.headers.month;
+				} else {
+					table.headers = scorecard.headers.week;
+				}
+			};
+		console.log($routeParams);
+		
+		// build table data
+		table.headers = scorecard.headers.week;
+		table.rows = scorecard.rows;
+		
+		// assign properties and methods to controller
+		ctrl.cardType = $routeParams.type;
+		ctrl.timeOptions = scorecard.options;
+		ctrl.optUpdate = optUpdate;
+		ctrl.table = table;
+	}
+	/* add controller */
+	angular.module('alccDash')
+		.controller('dashboard', DashboardController);
+}(angular));
