116492ec23df16e82842a7064b0ab2466d74105c
Chet Manley <joshua.gardner+github@hotmail.com>
2015-12-09 21:27:19 -0600

    Add expand attribute to directive

--- app/shared/flexGraph.js
+++ app/shared/flexGraph.js
@@ -1,8 +1,8 @@
 (function (angular, undefined) {
 	'use strict';
 	/* create directive */
-	function flexGraph(graphs) {
-		var Controller = function Controller() {
+	function flexGraph() {
+		var Controller = function Controller($scope, $mdDialog, graphs) {
 			var ctrl = this,
 				options = function setOptions() {
 					var opts = {};
@@ -15,27 +15,61 @@
 						break;
 					}
 					return opts;
+				},
+				showGraphModal = function showGraphModal($event) {
+					$mdDialog.show({
+						templateUrl: 'app/components/dashboard/graph.modal.htm',
+						controller: 'base.modal',
+						controllerAs: 'ctrl',
+						locals: {
+							name: ctrl.name,
+							header: ctrl.header
+						},
+						bindToController: true,
+						parent: angular.element(document.body),
+						openFrom: $scope.container,
+						closeTo: $scope.container,
+						clickOutsideToClose: true
+					});
+				},
+				init = function init() {
 					// request graphs
 					ctrl.graphs = graphs.get();
+					// set defaults
+					ctrl.type = ctrl.type || 'Line';
+					ctrl.header = ctrl.header || '';
 					ctrl.options = ctrl.graphs[ctrl.name].options || options();
+					// set public methods
+					ctrl.expand = showGraphModal;
 				};
-			ctrl.type = ctrl.type || 'Line';
-		};
+			// this controller auto-inits
+			init();
+		},
+			linkFn = function linkFn(scope, element, attrs) {
+				// link non-value attributes
+				if (attrs.hasOwnProperty('graphExpand')) {
+					scope.expandable = true;
+				}
+				// cache container element
+				scope.container = element[0];
+			};
+		Controller.$inject = ['$scope', '$mdDialog', 'graphs'];
 		
 		return {
 			restrict: 'AE',
 			templateUrl: 'app/shared/flexGraph.htm',
 			scope: {},
 			bindToController: {
-				'type': '@graphType',
 				'name': '@graphName',
-				'onClick': '=graphClick'
+				'type': '@?graphType',
+				'header': '@?graphHeader',
+				'onClick': '=?graphClick'
 			},
 			controller: Controller,
-			controllerAs: 'graphCtrl'
+			controllerAs: 'graphCtrl',
+			link: linkFn
 		};
 	}
-	flexGraph.$inject = ['graphs'];
 	/* add directive */
 	angular.module('alccDash')
 		.directive('flexGraph', flexGraph);
